58dac628ac74aeb9dbba0467d1a9d2a422cfec22
prat18 <pratyush181996@gmail.com>
2019-06-26 20:35:03 +0530

    Divide-and-Conquer

---
+++ Other Programs/Addition using Divide-and-Conquer/Recursive Addition 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Recursive_Addition_2
+{
+    class Program
+    {
+        class addition
+        {
+            public int RecursiveAdd(int[] A, int low, int high) // Line C
+            {
+                int sum = 0;
+                int mid;
+
+                if (low == high)
+                {
+                    return A[low];
+                }
+                else
+                {
+                    mid = (low + high) / 2;
+                    sum += RecursiveAdd(A, low, mid) + RecursiveAdd(A, mid + 1, high);
+                    return sum;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int high;
+
+            Console.WriteLine("Enter the number of inputs: ");
+            high = Convert.ToInt32(Console.ReadLine());
+            int low = 0;
+
+            int[] A = new int[high];
+
+            Console.WriteLine("Enter the numbber you want to add:");
+
+            for (int i = 0; i < high; i++)
+            {
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            addition a = new addition();
+            int result = a.RecursiveAdd(A, low, high-1); // Line A
+
+            Console.WriteLine("Sum: " + result);
+
+
+            Console.ReadKey();
+        }
+    }
+}
